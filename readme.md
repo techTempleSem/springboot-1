54110d96a39f4e035f4710125a031c5fb89b99fe
techTempleSem <techTempleSem@gmail.com>
2024-03-18 19:38:55 +0900

    get method

---
+++ readme.md
@@ -0,0 +1,7 @@
+# annotation
+
+-   @RestController : restApi 기능을 하는 클래스를 지정한다.
+
+-   @RequestMapping("/api") : 이 주소로 오는 일들을 처리한다.
+
+-   @GetMapping(path="/hello") : GET 메소드를 호출한다.
---
+++ src/main/java/com/example/restapi/controller/RestApiController.java
@@ -0,0 +1,15 @@
+package com.example.restapi.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class RestApiController {
+    @GetMapping(path="/hello")
+    public String hello(){
+        var html = "<html><body><h1>Hello Spring Boot</h1></body></html>";
+        return html;
+    }
+}
