4bef8ea14737070161ddac87e058f63f32bfee69
techTempleSem <techTempleSem@gmail.com>
2024-03-18 22:38:28 +0900

    path variable

--- readme.md
+++ readme.md
@@ -5,3 +5,20 @@
 -   @RequestMapping("/api") : 이 주소로 오는 일들을 처리한다.
 
 -   @GetMapping(path="/hello") : GET 메소드를 호출한다.
+
+## getMapping
+
+다음과 같은 식으로 변수를 넘길 수 있다. 이때 이름은 같아야 한다.
+```
+@GetMapping(path = "/echo/{message}")
+public String echo(
+        @PathVariable String message
+){
+```
+다른 이름을 쓰고 싶으면 @PathVariable(name="message")로 이름을 지정해 줄 수 있다.
+```
+@GetMapping(path = "/echo/{message}")
+public String echo(
+        @PathVariable(name="message") String msg
+){
+```
--- src/main/java/com/example/restapi/controller/RestApiController.java
+++ src/main/java/com/example/restapi/controller/RestApiController.java
@@ -1,6 +1,7 @@
 package com.example.restapi.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,5 +12,13 @@ public class RestApiController {
     public String hello(){
         var html = "<html><body><h1>Hello Spring Boot</h1></body></html>";
         return html;
+    }
+
+    @GetMapping(path = "/echo/{message}")
+    public String echo(
+            @PathVariable(name="message") String msg
+    ){
+        System.out.println("echo message : "+msg);
+        return msg;
     }
 }
